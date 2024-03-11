a49b2911aa67e7dfa56ac632bb706152661812c7
coreylo7 <coreylo@csus.edu>
2024-03-11 00:34:12 -0700

    test

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("301297555 - This line was added by Patrick Nguyen.");
-		System.out.println("218835332 - This line was added by Corey Lo.");
+		System.out.println("218835332 - This line was added by Corey Lo."); //
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
